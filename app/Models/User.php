d6f4b262f632c8117228429865bcf94e87e15561
Alexander Hill <ahill@synergylife.works>
2021-10-08 15:33:17 -0400

    Change protected authentication messages

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 
-        'remember_token',
+        'rememberme_token',
     ];
     
     public function getName(){
